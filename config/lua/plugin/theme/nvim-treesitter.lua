bab2a6fcea6477430c4bda51bbc4df42ec0ab0ac
sail <fogill@outlook.com>
2025-02-22 14:59:53 +0800

    v1.0.1 fix: treesitter.config

--- config/lua/plugin/theme/nvim-treesitter.lua
+++ config/lua/plugin/theme/nvim-treesitter.lua
@@ -2,7 +2,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     event = "VimEnter",
-    cofig = function()
+    config = function()
         require'nvim-treesitter.configs'.setup {
             -- A list of parser names, or "all" (the listed parsers MUST always be installed)
             ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
